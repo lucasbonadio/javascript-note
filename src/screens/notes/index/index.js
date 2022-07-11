e6360cd566215428ce4fff3854475895c373cd68
Lucas Bonadio <lucasbonadio2@hotmail.com>
2022-07-11 07:51:47 -0300

    Creating a new header when the user is logged and the function logout

--- src/screens/notes/index/index.js
+++ src/screens/notes/index/index.js
@@ -1,7 +1,9 @@
 import React, { Fragment } from 'react';
+import HeaderLogged from '../../../components/header_logged';
 
 const NotesIndexScreen = () => (
     <Fragment>
+        <HeaderLogged />
         Hello World
     </Fragment>
 );
