5695525b03830ff423ab4f1203fc20725f97da0f
Lucas Bonadio <lucasbonadio2@hotmail.com>
2022-07-12 03:20:26 -0300

    Creating sidebar notes

--- src/screens/notes/index/index.js
+++ src/screens/notes/index/index.js
@@ -1,11 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import HeaderLogged from '../../../components/header_logged';
+import Notes from '../../../components/notes';
+import { Column } from "rbx";
 
-const NotesIndexScreen = () => (
-    <Fragment>
-        <HeaderLogged />
-        Hello World
-    </Fragment>
-);
+const NotesIndexScreen = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <Fragment>
+            <HeaderLogged setIsOpen={setIsOpen} />
+            <Notes isOpen={isOpen} setIsOpen={setIsOpen} />
+        </Fragment>
+    );
+}
 
 export default NotesIndexScreen;
